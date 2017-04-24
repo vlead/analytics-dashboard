80a7c72457fb59e4924c203014c477dfef1b7644
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2017-04-24 09:54:48 +0530

    script for getting data from JSON

--- src/web/functions.js
+++ src/web/functions.js
@@ -1,20 +1,25 @@
 var ctx = document.getElementById("myChart");
 console.log(feedbacks.length);
-var m, responses, answers;
-var questions;
+document.write(feedbacks.length);
+var m, question_type, answer_type, n, version;
 var feedbackLen = feedbacks.length;
 document.write("hellooooo");
 var myChart = new Chart(ctx);
 {
   for (m = 0; m<feedbackLen; m++) {
-    //document.write(JSON.stringify(feedbacks));
     responses=feedbacks[m]['responses'];
-    questions=responses[m]['question'];
-    answers=responses[m]['answers'];
-    //answers=answers[m];
-    console.log(responses);
-    console.log(questions);
-    console.log(answers);
-     }
+    version=feedbacks[m]['version'];    
+    for (n = 0; n<responses.length; n++) {
+      //feedbacks[m]['responses'][n]['question'].setItem('name', 'ques');
+      question_type = feedbacks[m]['responses'][n]['question'].name;
+      answer_type=feedbacks[m]['responses'][n]['answers'][0].name;
+      console.log(version);
+      console.log(question_type);
+      console.log(JSON.stringify(answer_type));
+    }
+    
+   
+  }
+  document.write(n);
 }
 console.log(myChart);
