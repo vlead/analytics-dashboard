03cade44cb45757cb0a9876d3bf8f1b8d8efd6ab
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2017-04-25 14:24:24 +0530

    updated for adding versions

--- src/web/functions.js
+++ src/web/functions.js
@@ -4,6 +4,7 @@ document.write(feedbacks.length);
 var m, question_type, answer_type, n, version;
 var feedbackLen = feedbacks.length;
 var myChart = new Chart(ctx);
+var version_list = new Array();
 {
   for (m = 0; m<feedbackLen; m++) {
     responses=feedbacks[m]['responses'];
@@ -14,9 +15,21 @@ var myChart = new Chart(ctx);
       answer_type=feedbacks[m]['responses'][n]['answers'][0].name;
       console.log(version);
       console.log(question_type);
-      console.log(JSON.stringify(answer_type));
-    }   
+      console.log(JSON.stringify(answer_type));     
+      contains(feedbacks,version);
+    }
+    
   }
-  document.write(n);
+  function contains(feedbacks, version) {
+    for (var i = 0 ; i<feedbacks.length; i++) {
+      if (version_list.contains(version)){
+          break;
+        }
+        else {
+          version_list.push(version);
+          document.write(version_list);
+        }      
+      }
+  }
+  console.log(myChart);
 }
-console.log(myChart);
