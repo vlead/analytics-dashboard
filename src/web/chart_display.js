d2e6aa4080dae852032b0f9a4b3f0e54f9a47e3e
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2017-04-28 18:46:01 +0530

    updated 4 graphs with css

--- src/web/chart_display.js
+++ src/web/chart_display.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-$(function() {
-  $( "#tabs-1" ).tabs();
-});
-});
 var ctx = document.getElementById("myChart");
 var total_vlabs_generic_fbs = 0;
 var total_exp_fbs = 0;
@@ -51,20 +46,20 @@ $.getJSON(elastic_url, function(data) {
 		{
 		    label: "Feedbacks Dashboard",
 		    backgroundColor: [
-			'rgba(255, 99, 132, 0.2)',
-			'rgba(54, 162, 235, 0.2)',
-			'rgba(255, 206, 86, 0.2)',
-			'rgba(75, 192, 192, 0.2)',
-			'rgba(153, 102, 255, 0.2)',
-			'rgba(255, 159, 64, 0.2)'
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
 		    ],
 		    borderColor: [
-			'rgba(255,99,132,1)',
-			'rgba(54, 162, 235, 1)',
-			'rgba(255, 206, 86, 1)',
-			'rgba(75, 192, 192, 1)',
-			'rgba(153, 102, 255, 1)',
-			'rgba(255, 159, 64, 1)'
+		      'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
 		    ],
 		    borderWidth: 1,
 		    data: [total_edx_exp_fbs, total_edx_lab_fbs, total_lab_fbs, total_exp_fbs, total_vlabs_generic_fbs, total_generic_fbs],
@@ -119,20 +114,20 @@ var ctx = document.getElementById("versionChart");
 		{
 		    label: "Feedbacks Dashboard",
 		    backgroundColor: [
-			'rgba(255, 99, 132, 0.2)',
-			'rgba(54, 162, 235, 0.2)',
-			'rgba(255, 206, 86, 0.2)',
-			'rgba(75, 192, 192, 0.2)',
-			'rgba(153, 102, 255, 0.2)',
-			'rgba(255, 159, 64, 0.2)'
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
 		    ],
 		    borderColor: [
-			'rgba(255,99,132,1)',
-			'rgba(54, 162, 235, 1)',
-			'rgba(255, 206, 86, 1)',
-			'rgba(75, 192, 192, 1)',
-			'rgba(153, 102, 255, 1)',
-			'rgba(255, 159, 64, 1)'
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
 		    ],
 		    borderWidth: 1,
 		    data: [total_edx_exp_fbs, total_edx_lab_fbs, total_lab_fbs, total_exp_fbs, total_generic_fbs],
@@ -141,6 +136,139 @@ var ctx = document.getElementById("versionChart");
 	}
 	
     });
+var elastic_url = "http://10.4.12.46/vlabs/feedback/_search?q=version:open-edx-virtual-labs-v2.0_for_lab&size=10000";
+$.getJSON(elastic_url, function(data) {
+    total_feedbacks = data['hits']['hits'];
+    console.log(total_feedbacks);
+    console.log(total_feedbacks.length);
+    for(var i=0;i<total_feedbacks.length;i++){
+	if(total_feedbacks[i]['_source']['version'] == "open-edx-virtual-labs-v2.0_for_experiment"){
+	    total_edx_exp_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "open-edx-virtual-labs-v2.0_for_lab"){
+	    total_edx_lab_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "generic-lab-feedback-v2.0"){
+	    total_lab_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "generic-exp-feedback-v2.0"){
+	    total_exp_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "virtual-labs-feedback-v1.0"){
+	    total_generic_fbs += 1;	
+	}
+    }
+var ctx = document.getElementById("openEdxLabChart");    
+    var myBarChart = new Chart(ctx, {
+	type: 'bar',
+	data: {
+	    labels: ["open-edx-virtual-labs-v2.0_for_experiment", "open-edx-virtual-labs-v2.0_for_lab", "generic-lab-feedback-v2.0", "generic-exp-feedback-v2.0", "virtual-labs-feedback-v1.0"],
+	    datasets: [
+		{
+		    label: "Feedbacks Dashboard",
+		    backgroundColor: [
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
+		    ],
+		    borderColor: [
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
+		    ],
+		    borderWidth: 1,
+		    data: [total_edx_exp_fbs, total_edx_lab_fbs, total_lab_fbs, total_exp_fbs, total_generic_fbs],
+		}
+	    ]
+	}
+	
+    });
+    
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+    location.reload();
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+
+
+
+var elastic_url = "http://10.4.12.46/vlabs/feedback/_search?q=version:open-edx-virtual-labs-v2.0_for_experiment&size=10000";
+$.getJSON(elastic_url, function(data) {
+    total_feedbacks = data['hits']['hits'];
+    console.log(total_feedbacks);
+    console.log(total_feedbacks.length);
+    for(var i=0;i<total_feedbacks.length;i++){
+	if(total_feedbacks[i]['_source']['version'] == "open-edx-virtual-labs-v2.0_for_experiment"){
+	    total_edx_exp_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "open-edx-virtual-labs-v2.0_for_lab"){
+	    total_edx_lab_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "generic-lab-feedback-v2.0"){
+	    total_lab_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "generic-exp-feedback-v2.0"){
+	    total_exp_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "virtual-labs-feedback-v1.0"){
+	    total_generic_fbs += 1;	
+	}
+    }
+var ctx = document.getElementById("openEdxExpChart");    
+    var myBarChart = new Chart(ctx, {
+	type: 'bar',
+	data: {
+	    labels: ["open-edx-virtual-labs-v2.0_for_experiment", "open-edx-virtual-labs-v2.0_for_lab", "generic-lab-feedback-v2.0", "generic-exp-feedback-v2.0", "virtual-labs-feedback-v1.0"],
+	    datasets: [
+		{
+		    label: "Feedbacks Dashboard",
+		    backgroundColor: [
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
+		    ],
+		    borderColor: [
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
+		    ],
+		    borderWidth: 1,
+		    data: [total_edx_exp_fbs, total_edx_lab_fbs, total_lab_fbs, total_exp_fbs, total_generic_fbs],
+		}
+	    ]
+	}
+	
+    });
+    
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+    location.reload();
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
     
 })
   .done(function() {
