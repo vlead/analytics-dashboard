208054e7700c0479d39dcdc342e1f86ebaafb282
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2017-05-16 00:07:43 +0530

    updated tabs based on version

--- src/web/chart_display.js
+++ src/web/chart_display.js
@@ -136,6 +136,84 @@ var ctx = document.getElementById("versionChart");
 	}
 	
     });
+})
+.done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+    location.reload();
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+
+  //Generic exp chart
+var elastic_url = "http://10.4.12.46/vlabs/feedback/_search?q=version:virtual-labs-feedback-v1.0&size=10000";
+$.getJSON(elastic_url, function(data) {
+    total_feedbacks = data['hits']['hits'];
+    console.log(total_feedbacks);
+    console.log(total_feedbacks.length);
+    for(var i=0;i<total_feedbacks.length;i++){
+	if(total_feedbacks[i]['_source']['version'] == "open-edx-virtual-labs-v2.0_for_experiment"){
+	    total_edx_exp_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "open-edx-virtual-labs-v2.0_for_lab"){
+	    total_edx_lab_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "generic-lab-feedback-v2.0"){
+	    total_lab_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "generic-exp-feedback-v2.0"){
+	    total_exp_fbs += 1;	
+	}
+	else if(total_feedbacks[i]['_source']['version'] == "virtual-labs-feedback-v1.0"){
+	    total_generic_fbs += 1;	
+	}
+    }
+var ctx = document.getElementById("genericExpChart");    
+    var myBarChart = new Chart(ctx, {
+	type: 'bar',
+	data: {
+	    labels: ["open-edx-virtual-labs-v2.0_for_experiment", "open-edx-virtual-labs-v2.0_for_lab", "generic-lab-feedback-v2.0", "generic-exp-feedback-v2.0", "virtual-labs-feedback-v1.0"],
+	    datasets: [
+		{
+		    label: "Feedbacks Dashboard",
+		    backgroundColor: [
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
+		    ],
+		    borderColor: [
+			'rgba(255, 51, 51, 1)',
+			'rgba(14, 128, 154, 1)',
+			'rgba(255, 204, 0, 1)',
+			'rgba(51, 204, 51, 1)',
+			'rgba(0, 153, 225, 1)',
+			'rgba(255, 51, 153, 1)'
+		    ],
+		    borderWidth: 1,
+		    data: [total_edx_exp_fbs, total_edx_lab_fbs, total_lab_fbs, total_exp_fbs, total_generic_fbs],
+		}
+	    ]
+	}
+	
+    });
+})
+.done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+    location.reload();
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+
 var elastic_url = "http://10.4.12.46/vlabs/feedback/_search?q=version:open-edx-virtual-labs-v2.0_for_lab&size=10000";
 $.getJSON(elastic_url, function(data) {
     total_feedbacks = data['hits']['hits'];
@@ -257,18 +335,6 @@ var ctx = document.getElementById("openEdxExpChart");
 	}
 	
     });
-    
-})
-  .done(function() {
-    console.log( "second success" );
-  })
-  .fail(function() {
-    console.log( "error" );
-    location.reload();
-  })
-  .always(function() {
-    console.log( "complete" );
-  });
     
 })
   .done(function() {
